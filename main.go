39f3e833c356b089120f5c0be95dd323b07396b0
Mona Lisa <nikruzhok@gmail.com>
2022-11-17 14:06:47 +0300

    second

--- main.go
+++ main.go
@@ -4,70 +4,161 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"runtime"
+	"sync/atomic"
 	"time"
 )
 
 func main() {
-
+	rand.Seed(time.Now().UnixNano())
 	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	res, err := Map(data, func(k int) (int, error) {
 		rnd := rand.Intn(1000)
-		if rnd > 500 {
+		if rnd > 900 {
 			return rnd, errors.New("unknown error")
 		}
-		<-time.After(time.Duration(1000) * time.Millisecond)
+		<-time.After(time.Duration(rnd) * time.Millisecond)
+		Printf("DONE %v", k)
 		return rnd, nil
-	}, 1)
-	<-time.After(500)
+	}, 3)
+	<-time.After(time.Second)
 	fmt.Println("RESULT:", res, err)
+}
+
+func Printf(template string, rest ...interface{}) {
+	args := append([]interface{}{time.Now()}, rest...)
+	fmt.Printf("[%v]    "+template+"\n", args...)
 }
 
 func Map[K comparable, V any](list []K, f func(k K) (V, error), concurrency int) (map[K]V, error) {
 	if concurrency == 0 {
 		concurrency = len(list)
 	}
-	concurrency -= 1
-	traffic := make(chan struct{}, concurrency)
-	defer close(traffic)
-
-	ch := make(chan struct {
+	fmt.Println("CONCURRENCY:", concurrency)
+	errchan := make(chan error)
+	traffic := make(chan struct{}, concurrency-1)
+	outchan := make(chan struct {
 		Key   K
 		Value V
 		error
 	})
+
+	var stop int32
+	res := make(map[K]V, 0)
+
+	go func() {
+		defer close(errchan)
+		defer close(outchan)
+		for range list {
+			m := <-outchan
+			Printf("m := <-outchan: %v %v %v", m.Key, m.Value, m.error)
+			if m.error != nil {
+				errchan <- m.error
+				break
+			}
+			res[m.Key] = m.Value
+			fmt.Println(res, len(traffic))
+			<-traffic
+		}
+	}()
+
+	go func() {
+		for _, key := range list {
+			go func(key K) {
+				Printf("go func(key K) %v", key)
+				value, err := f(key)
+				Printf("VALUE %v %v", value, err)
+				if atomic.LoadInt32(&stop) != 0 {
+					return
+				}
+				outchan <- struct {
+					Key   K
+					Value V
+					error
+				}{key, value, err}
+			}(key)
+			Printf("traffic <- struct{}{} ... %v", len(traffic))
+			traffic <- struct{}{}
+			Printf("traffic <- struct{}{}")
+		}
+		fmt.Println("____________Dasdasd", len(traffic))
+		close(traffic)
+	}()
+
+	err := <-errchan
+
+	atomic.AddInt32(&stop, 1)
+
+	return res, err
+}
+
+func Map_[K comparable, V any](list []K, f func(k K) (V, error), concurrency int) (map[K]V, error) {
+	if concurrency == 0 {
+		concurrency = len(list)
+	}
+	fmt.Println("CONCURRENCY:", concurrency)
+	traffic := make(chan K, concurrency-1)
+	ch := make(chan struct {
+		Key   K
+		Value V
+		error
+	}, concurrency)
 	defer close(ch)
 
 	delta, total := time.Now(), time.Now()
 
-	for _, key := range list {
-		go func(key K) {
-			traffic <- struct{}{}
-			fmt.Println("   traffic <-\n")
-			value, err := f(key)
-			fmt.Printf("ch <- %v [Value=%v, Error=%v] (%v)\n", key, value, err, time.Now().Sub(delta))
-			ch <- struct {
-				Key   K
-				Value V
-				error
-			}{key, value, err}
-		}(key)
-	}
+	var stop int32
+
+	go func() {
+		for _, key := range list {
+			Printf("... traffic <- %v", key)
+			traffic <- key
+			Printf("    traffic <- %v", key)
+			if atomic.LoadInt32(&stop) != 0 {
+				break
+			}
+			go func(key K) {
+				Printf("go func(key K) %v", key)
+				value, err := f(key)
+				Printf("... ch <- %v [Value=%v, Error=%v] (%v)", key, value, err, time.Now().Sub(delta))
+				if atomic.LoadInt32(&stop) != 0 {
+					return
+				}
+				Printf("11111111111 %v %v", key, atomic.LoadInt32(&stop))
+				ch <- struct {
+					Key   K
+					Value V
+					error
+				}{key, value, err}
+				Printf("    ch <- %v [Value=%v, Error=%v] (%v)", key, value, err, time.Now().Sub(delta))
+			}(key)
+		}
+		close(traffic)
+		Printf("CLOOSE(CH)")
+	}()
 
 	res := make(map[K]V, 0)
 	for range list {
+		Printf(" <- traffic ...")
 		lock := <-traffic
-		fmt.Println("<- traffic", lock)
+		Printf(" <- traffic %v", lock)
+		Printf(" <- ch ...")
 		m := <-ch
-		fmt.Printf("<- ch %v [Value=%v, Error=%v]\n", m.Key, m.Value, m.error)
+		Printf(" <- ch %v [Value=%v, Error=%v]", m.Key, m.Value, m.error)
 		if m.error != nil {
-			fmt.Printf("%v [Value=%v, Error=%v] (%s)\n", m.error, m.Key, m.Value, time.Now().Sub(total))
-			fmt.Printf("len(traffic)=%v\n", len(traffic))
+			Printf("ERROR: %v [Key=%v, Value=%v] (%s)", m.error, m.Key, m.Value, time.Now().Sub(total))
+			Printf("len(traffic)=%v, NumGoroutine=%v", len(traffic), runtime.NumGoroutine())
+			atomic.AddInt32(&stop, 1)
+			Printf("STOP")
+			for range traffic {
+				Printf("%v %v", len(traffic), cap(traffic))
+			}
 			return nil, m.error
 		}
 		res[m.Key] = m.Value
 	}
 
-	fmt.Println(res, time.Now().Sub(total))
+	fmt.Println("RETURN:", res, time.Now().Sub(total))
 	return res, nil
 }
 
@@ -81,7 +172,7 @@ func Map2[K comparable, V any](list []K, f func(k K) (V, error)) ([]V, error) {
 		go func(i int, key K) {
 			value, _ := f(key)
 			promises[i] <- value
-			fmt.Printf("promises[%v] <- %v (%vms)\n", i, value, time.Now().Sub(delta))
+			Printf("promises[%v] <- %v (%vms)", i, value, time.Now().Sub(delta))
 			close(promises[i])
 		}(i, key)
 	}
@@ -90,7 +181,7 @@ func Map2[K comparable, V any](list []K, f func(k K) (V, error)) ([]V, error) {
 
 	for i := range list {
 		v := <-promises[i]
-		fmt.Printf("<- promises[%v] %v\n", i, v)
+		Printf("<- promises[%v] %v", i, v)
 		res[i] = v
 	}
 

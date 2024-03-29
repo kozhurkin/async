d5329d74a9c381419976a0caed01fb2c21211326
kozhurkin <dkozhurkin@gmail.com>
2024-03-30 00:12:01 +0300

    pipers v1.1.0

--- asyncPipers.go
+++ asyncPipers.go
@@ -9,9 +9,7 @@ import (
 // bench: ⚠️ too slow
 
 func AsyncPipers[A any, V any](ctx context.Context, args []A, f func(context.Context, int, A) (V, error), concurrency int) ([]V, error) {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	return pipers.FromArgs(args, func(i int, a A) (V, error) {
+	return pipers.FromArgsCtx(args, func(ctx context.Context, i int, a A) (V, error) {
 		return f(ctx, i, a)
 	}).Context(ctx).Concurrency(concurrency).Resolve()
 }

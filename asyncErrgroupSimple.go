60e421f6f55e52824a350a3a9f858ee5c9cfda55
kozhurkin <dkozhurkin@gmail.com>
2025-08-17 07:12:14 +0300

    AsyncErrgroupSimple

--- asyncErrgroupSimple.go
+++ asyncErrgroupSimple.go
@@ -18,12 +18,38 @@ func AsyncErrgroupSimple[A any, V any](ctx context.Context, args []A, f func(con
 	for i, arg := range args {
 		i, arg := i, arg // захват переменных
 		eg.Go(func() error {
-			value, err := f(ctx, i, arg)
-			if err != nil {
-				return err
+			if ctxErr := ctx.Err(); ctxErr != nil {
+				return ctxErr
 			}
-			res[i] = value
-			return nil
+			printDebug("Task %v started", i)
+
+			resultCh := make(chan struct {
+				value V
+				err   error
+			}, 1)
+
+			// Запускаем f в отдельной горутине
+			go func() {
+				v, err := f(ctx, i, arg)
+				resultCh <- struct {
+					value V
+					err   error
+				}{v, err}
+			}()
+
+			select {
+			case <-ctx.Done():
+				printDebug("Task %v canceled due to context: %v", i, ctx.Err())
+				return ctx.Err()
+			case r := <-resultCh:
+				if r.err != nil {
+					printDebug("Task %v finished with error: %v", i, r.err)
+					return r.err
+				}
+				res[i] = r.value
+				printDebug("Task %v finished successfully", i)
+				return nil
+			}
 		})
 	}
 
